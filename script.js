5cfb1676e031780c6e7ef487e5a84cc28621e142
乡ＴＯＢＹ乡 <80438941+umrbek1032@users.noreply.github.com>
2022-12-01 04:07:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.replainSettings = { id: '929b3021-b55e-4128-85a1-8814a377ac30' };
+(function (u) {
+    var s = document.createElement('script'); s.async = true; s.src = u;
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})('https://widget.replain.cc/dist/client.js');
